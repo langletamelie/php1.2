e50908e6f8ab114cf3d588bcde8530ece75c9b66
langletamelie <amelie.langlet@hotmail.com>
2018-07-25 18:42:31 +0200

    php1.2

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP 1.2</title>
+  </head>
+  <body>
+    <p>
+    <?php
+    $lastname = 'Lechat';
+    $firstname = 'Lucien';
+    $age = 11;
+    echo $lastname . ' ' . $firstname . ' ' . $age;
+     ?>
+</p>
+  </body>
+</html>
