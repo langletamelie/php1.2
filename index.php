2871dcdcc11b4c5dda72dee6c45f3a699d6b6bbc
langletamelie <amelie.langlet@hotmail.com>
2018-07-26 14:17:54 +0200

    ajout de commentaires

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <title>PHP 1.2</title>
-  </head>
-  <body>
-    <p>
+<head>
+  <meta charset="utf-8" />
+  <title>PHP 1.2</title>
+</head>
+<body>
+  <p>
     <?php
     $lastname = 'Lechat';
     $firstname = 'Lucien';
     $age = 11;
     echo $lastname . ' ' . $firstname . ' ' . $age;
-     ?>
-</p>
-  </body>
+    /*
+    <?= $lastname . ' ' . $firstname . ' ' . $age; ?>
+    */
+    ?>
+  </p>
+</body>
 </html>
